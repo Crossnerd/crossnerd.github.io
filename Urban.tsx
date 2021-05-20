0fb583f8194889117528d0d81cec4fab656848fa
Florian <florian.bringmann@posteo.de>
2021-05-20 16:08:43 +0200

    adding lanterns and garbage

---
+++ Urban.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Urban" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="tiles_png/sonstiges/urban_streets_c_by_schwarzenacht_d8cmhcy.png" width="512" height="512"/>
+</tileset>
