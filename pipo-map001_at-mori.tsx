89c5582068a56d91952d8780d4927838d8e3c0fb
Florian <florian.bringmann@posteo.de>
2021-05-20 11:47:13 +0200

    Übertragen der MuHa Daten
    Übertragen aller Daten aus dem Mutterhaus Repository in diesen Ordner

---
+++ pipo-map001_at-mori.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="pipo-map001_at-mori" tilewidth="32" tileheight="32" tilecount="6" columns="2">
+ <image source="tiles_png/Pipoya RPG World Tileset 48x48 40x40 32x32/32x32/[A]_type2/pipo-map001_at-mori.png" width="64" height="96"/>
+</tileset>
