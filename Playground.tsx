19e6f89c89841177597d7e0d80616e8afc75c166
Florian <florian.bringmann@posteo.de>
2021-05-20 12:51:36 +0200

    some changes

---
+++ Playground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Playground" tilewidth="32" tileheight="32" tilecount="81" columns="9">
+ <image source="tiles_png/structures/rpg_maker_playground_tile_by_ayene_chan_d4bhs61-export.png" width="288" height="288"/>
+</tileset>
---
+++ tiles_png/Camping.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Camping" tilewidth="32" tileheight="32" tilecount="500" columns="25">
+ <image source="sonstiges/Tilemap+Sprites-export.png" width="800" height="653"/>
+</tileset>
